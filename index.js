3095a41c420ec6877f7e6cb880031e3cdbba7d55
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-04-03 11:47:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ main.remove();
 
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.innerHTML = 'Abdulmajeed is the champion'
+newHeader.innerHTML = 'Abdulmajeed is the champion';
 document.body.appendChild(newHeader);
